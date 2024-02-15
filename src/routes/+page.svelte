ea4c38c359c3c0d8d1daf79087035aa440abbfdf
Zenfay <111913348+Zenfay@users.noreply.github.com>
2024-02-15 12:31:39 -0600

    Commit 1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import bg from '$lib/assets/NikePic.jpg';
+</script>
+
+<img src={bg}/>
+<h1>Fashion Finder</h1>
+<h2>Unique clothes for a unique you</h2>
+
+<style>
+    img {
+		/*position: absolute;*/
+		top: 0;
+		left: 0;
+		width: 100%;
+        /*height: 100%;*/
+		will-change: transform;
+        /* transform: translate(-50%, 0); */
+	}
+    h1 {
+        text-align: center;
+        color: black;
+        font-family: sans-serif;
+        font-size: 4em;
+    }
+    h2 {
+        text-align: center;
+        color: black;
+        font-family: sans-serif;
+        font-size: 1em;
+    }
+    
+</style>
+
